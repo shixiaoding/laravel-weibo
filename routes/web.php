533279e3bb05ed5a8c90f2a8f85479e551585d06
oliver.shi <oliver.shi@gaodun.com>
2018-02-01 19:19:06 +0800

    'xx'

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::get('login','SessionsController@create')->name('login');
 Route::post('login','SessionsController@store')->name('login');
 Route::delete('logout','SessionsController@destroy')->name('logout');
 
+
+
+
