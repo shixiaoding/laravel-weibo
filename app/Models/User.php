99678e17e5dcaeb1265ea0c18d98e24ca9801022
shiding <536336091@qq.com>
2018-02-05 18:05:42 +0800

    '重置密码'

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\ResetPassword;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -40,5 +41,10 @@ class User extends Authenticatable
         static::creating(function ($user){
             $user->activation_token = str_random(30);
         });
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPassword($token));
     }
 }
